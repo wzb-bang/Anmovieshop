6af5bf5c8e79039181d6e84dda8c10234e742e49
sicilly <634068240@qq.com>
2019-05-20 19:00:00 +0800

    增加：添加了主界面

--- app/src/main/java/com/gdou/movieshop/LoginActivity.java
+++ app/src/main/java/com/gdou/movieshop/LoginActivity.java
@@ -50,7 +50,6 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
         initView();
     }
 
