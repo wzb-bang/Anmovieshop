b8706d414485bd56de16b436b1cc99da46523b51
sicilly <634068240@qq.com>
2019-05-21 13:51:06 +0800

    滑动卡片展示所有电影

--- app/src/main/java/com/gdou/movieshop/adapter/MoviesAdapter.java
+++ app/src/main/java/com/gdou/movieshop/adapter/MoviesAdapter.java
@@ -1,14 +1,18 @@
 package com.gdou.movieshop.adapter;
 
+import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.gdou.movieshop.MovieInfo;
 import com.gdou.movieshop.R;
 
+import java.net.URI;
 import java.util.List;
 
 public class MoviesAdapter  extends RecyclerView.Adapter<MoviesAdapter.ContactViewHolder> {
@@ -32,15 +36,16 @@ public class MoviesAdapter  extends RecyclerView.Adapter<MoviesAdapter.ContactVi
 
         @Override
         public void onBindViewHolder(ContactViewHolder holder, int position) {
-//MovieInfoList中包含的都是MovieInfo类的对象
-//通过其get()方法可以获
+            //MovieInfoList中包含的都是MovieInfo类的对象
+            //通过其get()方法可以获
             MovieInfo ci = MovieInfoList.get(position);
-//将viewholder中hold住的各个view与数据源进行绑定(bind)
+            //将viewholder中hold住的各个view与数据源进行绑定(bind)
 
-            holder.vName.setText(ci.getNamePrefix() + ci.getName());
-            holder.vSurname.setText(ci.getSurname() +ci.getSurname());
-            holder.vEmail.setText(ci.getEmail() + ci.getEmail());
-            holder.vTitle.setText(ci.getEmail() + "" + ci.getName());
+            holder.vName.setText(ci.getNamePrefix() + ci.getMovie_name());
+            holder.vScore.setText(ci.getScorePrefix() +ci.getMovie_score());
+            holder.vActor.setText(ci.getActorPrefix() + ci.getActor());
+            holder.vImage.setImageDrawable(ci.getImage());
+//            holder.vButton.setId(ci.getBtIdPrefix()+ci.getBt_id());
         }
 
         //此方法返回列表项的数目
@@ -51,16 +56,18 @@ public class MoviesAdapter  extends RecyclerView.Adapter<MoviesAdapter.ContactVi
     class ContactViewHolder extends RecyclerView.ViewHolder {
         //create the viewHolder class
         protected TextView vName;
-        protected TextView vSurname;
-        protected TextView vEmail;
-        protected TextView vTitle;
+        protected TextView vScore;
+        protected TextView vActor;
+        protected ImageView vImage;
+//        protected Button vButton;
 
         public ContactViewHolder(View itemView) {
             super(itemView);
-            vName = itemView.findViewById(R.id.text_name);
-            vSurname = itemView.findViewById(R.id.text_surname);
-            vEmail = itemView.findViewById(R.id.text_email);
-            vTitle = itemView.findViewById(R.id.title);
+            vName = itemView.findViewById(R.id.movie_name);
+            vScore = itemView.findViewById(R.id.movie_score);
+            vActor = itemView.findViewById(R.id.actor);
+            vImage=itemView.findViewById(R.id.card_movie_img1);
+//            vButton=itemView.findViewById(R.id.btn_buy1);
         }
     }
 }
